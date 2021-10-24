889bbabf966b60f31692381e986b5210dfe026f0
Gabriel Trindade <icandoitgrt@gmail.com>
2021-10-24 19:41:05 -0300

    consumindo api da covid

--- Atividades/A5/js/main.js
+++ Atividades/A5/js/main.js
@@ -1,5 +1,5 @@
 import Mascara from "./modules/masks.js";
-import { consultarCep } from "./modules/api.js";
+import { consultarCep, covidBrasilApi } from "./modules/api.js";
 
 const button = document.querySelector("#button");
 const cpfInput = document.querySelector("#input-cpf");
@@ -7,12 +7,40 @@ const cpfInput = document.querySelector("#input-cpf");
 button.addEventListener("click", (e) => {
   e.preventDefault();
 
-  Promise.all([consultarCep(cpfInput.value)]).then((data) => {
-    data = Object.values(data[0])
-    document.querySelector('#span_estado').innerHTML = data[5];
+  //   Promise.all([consultarCep(cpfInput.value)]).then((data) => {
+  //     data = Object.values(data[0])
+  //     document.querySelector('#span_estado').innerHTML = data[5];
+  //   });
+
+  Promise.all([consultarCep(cpfInput.value)]).then((dataCep) => {
+    dataCep = Object.values(dataCep[0]);
+    let uf = dataCep[5];
+    Promise.all([covidBrasilApi(uf)]).then((dataCovid) => {
+      preencherResultados(dataCovid);
+    });
   });
 });
 
 cpfInput.addEventListener("input", (e) => {
   e.target.value = Mascara.cep(e.target.value);
 });
+
+function preencherResultados(data) {
+  data = data[0];
+  document.querySelector("#span_estado").innerHTML = data.state;
+  document.querySelector("#span_casos").innerHTML = data.cases;
+  document.querySelector("#span_mortes").innerHTML = data.deaths;
+  document.querySelector("#span_suspeitos").innerHTML = data.suspects;
+  document.querySelector("#span_recusados").innerHTML = data.refuses;
+  document.querySelector("#span_atualizacao").innerHTML = formatarDate(data.datetime);
+}
+
+function formatarDate(value) {
+  const date = new Date(value);
+  let day = date.getDate();
+  let month = (date.getMonth() + 1);
+  let year = date.getFullYear();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  return `${day}/${month}/${year} às ${hours}:${minutes} (Horário de Brasilia)`;
+}
--- Atividades/A5/js/modules/api.js
+++ Atividades/A5/js/modules/api.js
@@ -6,6 +6,19 @@ export async function consultarCep(value) {
     const responseResult = await response.json();
     return responseResult;
   } catch (error) {
-    console.log("Erro ao consultar CEP!\n" + error);
+    console.log(error);
+    alert("Erro ao consultar CEP na API! Lembre-se, informe um CEP válido.");
+  }
+}
+
+export async function covidBrasilApi(value) {
+  try {
+    const url = `https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${value}`;
+    const response = await fetch(url);
+    const responseResult = await response.json();
+    return responseResult;
+  } catch (error) {
+    console.log(error);
+    alert("Ocorreu um erro ao consultar na Covid19 Brazil API!");
   }
 }
