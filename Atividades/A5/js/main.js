c15416ac4880c734c67f46c7c07ed1cd335fa7b7
Gabriel Trindade <icandoitgrt@gmail.com>
2021-10-24 19:54:21 -0300

    refatoração

--- Atividades/A5/js/main.js
+++ Atividades/A5/js/main.js
@@ -6,21 +6,15 @@ const cpfInput = document.querySelector("#input-cpf");
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
-
-  //   Promise.all([consultarCep(cpfInput.value)]).then((data) => {
-  //     data = Object.values(data[0])
-  //     document.querySelector('#span_estado').innerHTML = data[5];
-  //   });
-
   Promise.all([consultarCep(cpfInput.value)]).then((dataCep) => {
     dataCep = Object.values(dataCep[0]);
     let uf = dataCep[5];
-    if(uf != undefined){
-        Promise.all([covidBrasilApi(uf)]).then((dataCovid) => {
-            preencherResultados(dataCovid);
-          });
-    }else{
-        alert('CEP incorreto!');
+    if (uf != undefined) {
+      Promise.all([covidBrasilApi(uf)]).then((dataCovid) => {
+        preencherResultados(dataCovid);
+      });
+    } else {
+      alert("CEP incorreto!");
     }
   });
 });
@@ -36,13 +30,15 @@ function preencherResultados(data) {
   document.querySelector("#span_mortes").innerHTML = data.deaths;
   document.querySelector("#span_suspeitos").innerHTML = data.suspects;
   document.querySelector("#span_recusados").innerHTML = data.refuses;
-  document.querySelector("#span_atualizacao").innerHTML = formatarDate(data.datetime);
+  document.querySelector("#span_atualizacao").innerHTML = formatarDate(
+    data.datetime
+  );
 }
 
 function formatarDate(value) {
   const date = new Date(value);
   let day = date.getDate();
-  let month = (date.getMonth() + 1);
+  let month = date.getMonth() + 1;
   let year = date.getFullYear();
   let hours = date.getHours();
   let minutes = date.getMinutes();
