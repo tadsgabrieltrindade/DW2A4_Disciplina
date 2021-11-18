784920e9edb65166a477fc2bac56e2966119f441
Gabriel Trindade <icandoitgrt@gmail.com>
2021-11-18 10:17:05 -0300

    Criação de API/Banco de dados utilizando o JSON-Server para o formulário

--- projeto-spa/src/components/form/Select.js
+++ projeto-spa/src/components/form/Select.js
@@ -4,11 +4,16 @@ function Select({ text, name, options, handOnChange, value }) {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}:</label>
-      
-      <select name={name} id={name}>
-            <option> Selecione uma opção</option>
-      </select>
 
+      <select name={name} id={name}>
+        <option> Selecione uma opção</option>
+
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>
+            {option.name}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
--- projeto-spa/src/components/project/ProjectForm.js
+++ projeto-spa/src/components/project/ProjectForm.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react'
+
 import Input from "../form/Input";
 import Select from "../form/Select";
 import styles from "./ProjectFom.module.css";
 import SubmitButton from "../form/SubmitButton";
 
 function ProjectForm({ btnText }) {
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    
+  fetch("http://localhost:5000/categories", {
+    method: "GET",
+    headers: {
+      'Content-Type' : 'application/json'
+    }
+  }, [])
+  .then((resp) => resp.json())
+  .then((data )=> {setCategories(data)})
+  .catch((err) => {console.log(err)})
+  }, [])
+
   return (
     <form className={styles.form}>
       <Input
@@ -19,7 +37,7 @@ function ProjectForm({ btnText }) {
         placeholder="Insira o orçamento total"
       />
     
-    <Select name="category_id" text="Selecione a categoria"  />
+    <Select name="category_id" text="Selecione a categoria"  options={categories}/>
 
     <SubmitButton text={btnText} />
      
